0fe3db7e5e0b9e911ab990559d38b4f610a86893
unknown <simona_stolnicu@yahoo.com>
2018-05-25 23:09:12 +0300

    created basic controller Servlet.java

--- backend/src/main/java/com/uaic/info/tw/backend/Controller/Servlet/Servlet.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/Servlet/Servlet.java
@@ -1,5 +1,16 @@
-package com.uaic.info.tw.backend.Controller.Servlet;
+import java.io.IOException;
+import java.io.OutputStream;
 
-public class Servlet {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
+public class Servlet  implements HttpHandler{
+
+	public void handle(HttpExchange exchange) throws IOException {
+		String response = "This is the response";
+		exchange.sendResponseHeaders(200, response.length());
+		OutputStream os = exchange.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+	}
 }
