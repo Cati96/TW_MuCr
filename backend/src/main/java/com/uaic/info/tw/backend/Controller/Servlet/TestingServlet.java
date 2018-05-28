279c16429e7e94bab30388cba5fc43e3c6338825
Chilaboc Ecaterina Mihaela <c_ecaterina_mihaela96@yahoo.com>
2018-05-28 10:05:42 +0300

    Added clases for needed sevlets.

--- backend/src/main/java/com/uaic/info/tw/backend/Controller/Servlet/TestingServlet.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/Servlet/TestingServlet.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class Servlet  implements HttpHandler{
+public class TestingServlet  implements HttpHandler{
 
 	public void handle(HttpExchange exchange) throws IOException {
 		String response = "This is the response";
--- backend/src/main/java/com/uaic/info/tw/backend/Controller/WebServer.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/WebServer.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.sun.net.httpserver.HttpServer;
-import com.uaic.info.tw.backend.Controller.Servlet.Servlet;
+import com.uaic.info.tw.backend.Controller.Servlet.TestingServlet;
 import com.uaic.info.tw.backend.Globals.Variables;
 
 public class WebServer {
@@ -16,45 +16,8 @@ public class WebServer {
 
 	public WebServer() throws Exception {
 		server = HttpServer.create(new InetSocketAddress(Variables.SERVERPORT), 0);
-		server.createContext("/test", new Servlet());//creates a servlet on path test with class 
+		server.createContext("/test", new TestingServlet());//creates a servlet on path test with class 
 		server.setExecutor(null); // creates a default executor
 		server.start();
-	}
-	public static void parseQuery(String query, Map<String, 
-		Object> parameters) throws UnsupportedEncodingException {
-	
-         if (query != null) {
-             String pairs[] = query.split("[&]");
-             for (String pair : pairs) {
-                  String param[] = pair.split("[=]");
-                  String key = null;
-                  String value = null;
-                  if (param.length > 0) {
-                  key = URLDecoder.decode(param[0], 
-                  	System.getProperty("file.encoding"));
-                  }
-
-                  if (param.length > 1) {
-                           value = URLDecoder.decode(param[1], 
-                           System.getProperty("file.encoding"));
-                  }
-
-                  if (parameters.containsKey(key)) {
-                           Object obj = parameters.get(key);
-                           if (obj instanceof List<?>) {
-                                    List<String> values = (List<String>) obj;
-                                    values.add(value);
-
-                           } else if (obj instanceof String) {
-                                    List<String> values = new ArrayList<String>();
-                                    values.add((String) obj);
-                                    values.add(value);
-                                    parameters.put(key, values);
-                           }
-                  } else {
-                           parameters.put(key, value);
-                  }
-             }
-         }
 	}
 }
