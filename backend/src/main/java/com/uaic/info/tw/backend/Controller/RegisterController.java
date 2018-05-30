63e43111b2bd6922332e813c30bf3e4602cbe198
Chilaboc Ecaterina Mihaela <c_ecaterina_mihaela96@yahoo.com>
2018-05-30 13:04:00 +0300

    Attempt of creating register servlet

--- backend/src/main/java/com/uaic/info/tw/backend/Controller/RegisterController.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/RegisterController.java
@@ -1,5 +1,29 @@
 package com.uaic.info.tw.backend.Controller;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.uaic.info.tw.backend.Controller.Database.CRUDController;
+
 public class RegisterController {
+	Map<String, String> receivedParams = new HashMap<String, String>();
 	
+	CRUDController crudController;
+	
+	public RegisterController(Map<String,String> receivedParams) {
+		for(Map.Entry<String, String> item : receivedParams.entrySet()) {
+			this.receivedParams.put(item.getKey(), item.getValue());
+		}
+		
+		crudController = new CRUDController();
+	}
+	
+	public String verifyUsernameExistance() throws SQLException {
+		String response = "";
+		
+		String newUsername = receivedParams.get("username");
+		
+		return response;
+	}
 }
--- backend/src/main/java/com/uaic/info/tw/backend/Controller/WebServer.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/WebServer.java
@@ -1,13 +1,9 @@
 package com.uaic.info.tw.backend.Controller;
 
-import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import com.sun.net.httpserver.HttpServer;
+import com.uaic.info.tw.backend.Controller.Servlet.RegisterServlet;
 import com.uaic.info.tw.backend.Controller.Servlet.TestingServlet;
 import com.uaic.info.tw.backend.Globals.Variables;
 
@@ -16,7 +12,8 @@ public class WebServer {
 
 	public WebServer() throws Exception {
 		server = HttpServer.create(new InetSocketAddress(Variables.SERVERPORT), 0);
-		server.createContext("/test", new TestingServlet());//creates a servlet on path test with class 
+		server.createContext("/test", new TestingServlet());//creates a servlet on path test with class
+		server.createContext("/register", new RegisterServlet());
 		server.setExecutor(null); // creates a default executor
 	}
 	
