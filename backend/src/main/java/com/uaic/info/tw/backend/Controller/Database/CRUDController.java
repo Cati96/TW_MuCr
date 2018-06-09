dfc1a725f2a0c6ccf05bada8f5d2d79a7cb84460
unknown <bejan.octavian@yahoo.ro>
2018-06-09 14:16:11 +0300

    Am adaugat SaveDataController si SaveDataServlet si in plus am modificat CRUDController-ul si WebServer-ul

--- backend/src/main/java/com/uaic/info/tw/backend/Controller/Database/CRUDController.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/Database/CRUDController.java
@@ -85,4 +85,13 @@ public class CRUDController {
 		
 		return response;
 	}
+
+	public void saveUserGameData(int userId, String data) throws SQLException {
+		String query = "UPDATE SAVE_DATA SET save_data = ? where user_id = ?";
+		PreparedStatement pstm = Variables.DB_CONN.prepareStatement(query);
+		pstm.setString(1, data);
+		pstm.setInt(2, userId);
+		
+		pstm.executeUpdate();
+	}
 }
