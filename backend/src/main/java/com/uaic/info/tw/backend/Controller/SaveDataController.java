73a56fb22fdaee34307f1e0b46f96d7650f7a551
unknown <bejan.octavian@yahoo.ro>
2018-06-10 11:37:35 +0300

    Modified CRUDCOntroler, SaveDataController and Servlet

--- backend/src/main/java/com/uaic/info/tw/backend/Controller/SaveDataController.java
+++ backend/src/main/java/com/uaic/info/tw/backend/Controller/SaveDataController.java
@@ -1,5 +1,26 @@
 package com.uaic.info.tw.backend.Controller;
 
-public class SaveDataController {
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
+public class SaveDataController {
+Map<String, String> receivedParams = new HashMap<String, String>();
+	
+	CRUDController crudController;
+	
+	public SaveDataController(Map<String,String> receivedParams) {
+		for(Map.Entry<String, String> item : receivedParams.entrySet()) {
+			this.receivedParams.put(item.getKey(), item.getValue());
+		}
+		
+		crudController = new CRUDController();
+	}
+	
+	public void saveUserStatusGame() throws SQLException {		
+		int userId = Integer.parseInt(receivedParams.get("userId"));
+		String toSave = receivedParams.get("saveData");
+		
+		crudController.saveUserGameData(userId, toSave);
+	}
 }
