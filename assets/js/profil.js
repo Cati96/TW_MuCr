548686f70221729483912d83b5a94a2da25872d3
unknown <simona_stolnicu@yahoo.com>
2018-06-12 12:16:28 +0300

    Updated profil.js

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -1,26 +1,37 @@
-/*Using jikan api at https://jikan.docs.apiary.io/#*/
+/*Using kitsu api at https://kitsu.io/api/*/
 var jucator = new Object();
 jucator.resurse = resurse;
-jucator.profil = 40450;
+jucator.profil = 2;
 jucator.nume = "Nu este setat";
 jucator.id = 0;
 jucator.info = {};
 
 function populateAnime(id) {
-	var x = $.getJSON(" https://private-anon-11bdd5b450-jikan.apiary-proxy.com/character/" + id + "/pictures", function (data) {});
-	x.complete(function (data) {
-		jucator.info = data.responseJSON;
-		$("#showProfil").click(function () {
-			$("#numej").html(jucator.nume);
-			$("#numep").html(jucator.info.name);
-			$("#povp").html(jucator.info.about);
-			var temp_name = jucator.info.image;
-			var temp_html = '';
-			for (var i = 0; i < temp_name.length; i++) {
-				temp_html += '<img src="' + temp_name[i] + '" class="imagine_profil"/>';
-			}
-			$("#pozec").html(temp_html);
+	
+	var jucatorTemp='';
+	
+	$.get("http://127.0.0.1:8111/login",{
+		profil: id
+	},function(data1){
+		
+		jucatorTemp=data1;
+		console.log(jucatorTemp);
+		var x = $.getJSON("https://kitsu.io/api/edge/characters/" + jucatorTemp.split("<>|<>")[2], function (data) {});	
+		
+		x.complete(function (data) {
+			jucator.info = data.responseJSON.data;
+			$("#showProfil").click(function () {
+				$("#numej").html(jucatorTemp.split("<>|<>")[1]);
+				$("#numep").html(jucator.info.attributes.names.en);
+				$("#povp").html(jucator.info.attributes.description);
+				
+				var temp_name = jucator.info.attributes.image.original;
+				var temp_html = '';
+				temp_html += '<img src="' + temp_name + '" class="imagine_profil"/>';
+				$("#pozec").html(temp_html);
 		});
 	});
+	});
+	
 }
-populateAnime(jucator.profil);
+populateAnime($.cookie("userId"));
